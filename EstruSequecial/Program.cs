7d8589b9bc0e9d0ce6443f9ff2a60aefa6095805
n4rin <aline.soares2712@gmail.com>
2022-03-16 17:30:09 -0300

    Exercicio 4, estrutura sequencial

---
+++ EstruSequecial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstruSequecial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EstruSequecial/Solucao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstruSequecial
+{
+    class Solucao
+    {
+        public void ex4() 
+        {
+            int numFuncio, hrsTrab;
+            double valorHr, calcSalario;
+
+            Console.WriteLine("Exercicio 4!!\n");
+            Console.WriteLine("Digite o  numero do funcionario: \n");
+            numFuncio = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Digite as horas trabalhadas: \n");
+            hrsTrab = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Digite quanto recebe por hora: \n");
+
+        }
+        
+        
+    
+
+
+
+    }
+}
