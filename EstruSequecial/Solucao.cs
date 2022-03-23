3d0832ec329c312171223657360c982304c3f91c
n4rin <aline.soares2712@gmail.com>
2022-03-22 21:37:06 -0300

    add ex4

--- EstruSequecial/Solucao.cs
+++ EstruSequecial/Solucao.cs
@@ -11,19 +11,24 @@ namespace EstruSequecial
             int numFuncio, hrsTrab;
             double valorHr, calcSalario;
 
+            {
             Console.WriteLine("Exercicio 4!!\n");
             Console.WriteLine("Digite o  numero do funcionario: \n");
             numFuncio = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Digite as horas trabalhadas: \n");
             hrsTrab = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite quanto recebe por hora: \n");
+            Console.WriteLine("Digite o valor que recebe por hora: \n");
+            valorHr = double.Parse(Console.ReadLine());
+            Console.WriteLine("Seu salário será de: \n");
+            calcSalario = double.Parse(Console.ReadLine());
+            
+            calcSalario = (hrsTrab * valorHr);
+            Console.WriteLine("O salário do funcionario {1} é de {0:F2}: ", numFuncio, calcSalario);
+
+            }
 
         }
         
-        
-    
-
-
 
     }
 }
