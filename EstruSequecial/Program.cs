5f3b413e8a31a02042c6d009846a1297f291b3c1
n4rin <aline.soares2712@gmail.com>
2022-03-28 21:27:16 -0300

    ex04

--- EstruSequecial/Program.cs
+++ EstruSequecial/Program.cs
@@ -6,7 +6,21 @@ namespace EstruSequecial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numFuncio, hrsTrab;
+            double valorHr, calcSalario;
+
+            Console.WriteLine("Digite o  numero do funcionario: \n");
+            numFuncio = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite as horas trabalhadas: \n");
+            hrsTrab = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor que recebe por hora: \n");
+            valorHr = doule.Parse(Console.ReadLine());
+            Console.WriteLine("Seu salário será de: \n");
+            calcSalario = double.Parse(Console.ReadLine());
+            
+            calcSalario = hrsTrab * valorHr;
+            Console.WriteLine("O salário do funcionario {1} é de {0:F2}: ", numFuncio, calcSalario);
+
         }
     }
 }
--- EstruSequecial/Solucao.cs
+++ EstruSequecial/Solucao.cs
@@ -1,34 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace EstruSequecial
 {
     class Solucao
-    {
-        public void ex4() 
-        {
-            int numFuncio, hrsTrab;
-            double valorHr, calcSalario;
-
-            {
-            Console.WriteLine("Exercicio 4!!\n");
-            Console.WriteLine("Digite o  numero do funcionario: \n");
-            numFuncio = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite as horas trabalhadas: \n");
-            hrsTrab = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o valor que recebe por hora: \n");
-            valorHr = double.Parse(Console.ReadLine());
-            Console.WriteLine("Seu salário será de: \n");
-            calcSalario = double.Parse(Console.ReadLine());
-            
-            calcSalario = (hrsTrab * valorHr);
-            Console.WriteLine("O salário do funcionario {1} é de {0:F2}: ", numFuncio, calcSalario);
-
-            }
-
-        }
-        
-
-    }
+   
 }
