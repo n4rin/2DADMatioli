d856e60291c13399e54e959d0172d407c02b6697
n4rin <aline.soares2712@gmail.com>
2022-03-28 21:33:50 -0300

    add ex04

--- EstruSequecial/Program.cs
+++ EstruSequecial/Program.cs
@@ -6,19 +6,19 @@ namespace EstruSequecial
     {
         static void Main(string[] args)
         {
-            int numFuncio, hrsTrab;
+            int numFuncio, hrsTraba;
             double valorHr, calcSalario;
 
             Console.WriteLine("Digite o  numero do funcionario: \n");
             numFuncio = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite as horas trabalhadas: \n");
-            hrsTrab = int.Parse(Console.ReadLine());
+            hrsTraba = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o valor que recebe por hora: \n");
             valorHr = doule.Parse(Console.ReadLine());
             Console.WriteLine("Seu salário será de: \n");
             calcSalario = double.Parse(Console.ReadLine());
             
-            calcSalario = hrsTrab * valorHr;
+            calcSalario = hrsTraba * valorHr;
             Console.WriteLine("O salário do funcionario {1} é de {0:F2}: ", numFuncio, calcSalario);
 
         }
