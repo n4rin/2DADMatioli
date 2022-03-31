8e41e0c3bc3abe7137d439fb7082339a8272a962
vxpresto <nallinguilherme@gmail.com>
2022-03-30 22:33:13 -0300

    add ex06

---
+++ EstrSequencialExercicios/Ex06/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ex06 {
+  class Ex06 {
+    static void Main(string[] args) {
+      float a, b, c;
+      float areaTriang, areaCirc, areaTrapezio, areaquad, arearet;
+      
+      Console.WriteLine("Digite um numero: ");
+      a = float.Parse(Console.ReadLine());
+      Console.WriteLine("Digite outro numero: ");
+      b = float.Parse(Console.ReadLine());
+      Console.WriteLine("Digite mais um numero: ");
+      c = float.Parse(Console.ReadLine());
+
+      areaTriang = (a * c) / 2;
+      areaCirc = (float)(c * 3.14159);
+      areaTrapezio = ((a + b) / 2);
+      areaquad = b * b;
+      arearet = a * b;
+      
+      Console.WriteLine("A area do circulo é " + areaCirc);
+      Console.WriteLine("A area do trapézio é: " + areaTrapezio);
+      Console.WriteLine("A area do quadrado é " + areaquad);
+      Console.WriteLine("A area do retangulo é: " + arearet);
+    }
+  }
+}
